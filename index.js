ea4e9300d1166cd0d275b6f973437f80a0eb61f7
Troy Dorman <troydorman@gmail.com>
2021-09-18 20:32:24 -0600

    fired up basic express server

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const fetch = require('node-fetch');
-const { stringify } = require('querystring');
+const express = require("express");
+const bodyParser = require("body-parser");
+const fetch = require("node-fetch");
+const { stringify } = require("querystring");
+
+app.use(express.json());
+
+app.get("/", (_, res) => res.sendFile(__dirname + "/index.html"));
+
+app.listen(3000, () => console.log("Server started on port 3000"));
